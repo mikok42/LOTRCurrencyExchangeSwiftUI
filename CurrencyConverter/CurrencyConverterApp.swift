bc4dbfe0a8905121821545a1e2e0ce35e56c3f8f
Mikołaj Linczewski <mlinczewski@deloittece.com>
2023-01-02 09:38:52 +0100

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Linczewski, Mikolaj on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
